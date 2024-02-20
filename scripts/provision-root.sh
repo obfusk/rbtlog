207980e7e1f6f40aa20817bc3e37e619294f6ac4
FC (Fay) Stegerman <flx@obfusk.net>
2024-02-20 23:17:59 +0100

    provision-root.sh: support adduser from bullseye

--- scripts/provision-root.sh
+++ scripts/provision-root.sh
@@ -9,6 +9,11 @@ apt-get install --no-install-recommends -y git wget unzip "${PROVISIONING_JDK}" 
   "${extra_packages[@]}"
 update-alternatives --auto java
 
-adduser --disabled-password --comment '' "${BUILD_USER}" --home "${BUILD_HOME_DIR}"
+if adduser --help | grep -q -- --gecos; then
+  adduser --disabled-password --gecos '' "${BUILD_USER}" --home "${BUILD_HOME_DIR}"
+else
+  adduser --disabled-password --comment '' "${BUILD_USER}" --home "${BUILD_HOME_DIR}"
+fi
+
 mkdir -p "${ANDROID_HOME}"
 chown "${BUILD_USER}:${BUILD_USER}" "${ANDROID_HOME}"
