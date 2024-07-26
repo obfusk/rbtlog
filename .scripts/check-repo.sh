7d87ae42f7e5d3b95ea72c4ec71ef92da6c5bec9
FC (Fay) Stegerman <flx@obfusk.net>
2024-07-27 01:56:26 +0200

    improve check-repo.sh

--- .scripts/check-repo.sh
+++ .scripts/check-repo.sh
@@ -6,6 +6,6 @@ for x in $( git log --format=%H log ); do git verify-commit "$x"; done
 for x in $( git tag ); do git verify-tag "$x"; done
 
 git diff --exit-code code master ':!recipes/*.yml'
-git diff --exit-code master log ':!logs/*.json' ':!*.json'
+git diff --exit-code master log ':!logs/*.json' ':![i]ndex.json' ':![a]bout.json'
 
 echo OK
